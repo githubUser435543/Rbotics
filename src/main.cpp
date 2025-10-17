9ed6aba742404d72890be09cb26c27aa73332c52
Jacob Wesson <jacobwesson434@gmail.com>
2025-10-17 16:24:45 -0400

    removed commented out opcontrol

--- src/main.cpp
+++ src/main.cpp
@@ -279,7 +279,7 @@ void opcontrol() {
 			drivetrain.startRunningIntake();
 			drivetrain.stopOuttake();
 		} else if (l2){
-			drivetrain.startRunningIntake(false);
+			drivetrain.startRunningIntake(true);
 			drivetrain.stopOuttake();
 		} else {
 			drivetrain.stopIntake();
@@ -294,32 +294,10 @@ void opcontrol() {
 			drivetrain.moveIntakeArm();
 		}
 
+
 		pros::lcd::print(0 /*line*/, "Mode: %d", static_cast<int>(drivetrain.getOpcontrolMode()));
 
 		drivetrain.opcontrolLoop(master);
 		pros::delay(20);
 	}
 }
-
-/*
-void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::MotorGroup left_mg({1, -2, 3});    // Creates a motor group with forwards ports 1 & 3 and reversed port 2
-	pros::MotorGroup right_mg({-4, 5, -6});  // Creates a motor group with forwards port 5 and reversed ports 4 & 6
-
-
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);  // Prints status of the emulated screen LCDs
-
-		// Arcade control scheme
-
-		int dir = master.get_analog(ANALOG_LEFT_Y);    // Gets amount forward/backward from left joystick
-		int turn = master.get_analog(ANALOG_RIGHT_X);  // Gets the turn left/right from right joystick
-		left_mg.move(dir - turn);                      // Sets left motor voltage
-		right_mg.move(dir + turn);                     // Sets right motor voltage
-		pros::delay(20);                               // Run for 20 ms then update
-	}
-}
-*/
